e2d14ec4c98905456c3cb00c133951e67959d4e5
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-06-21 18:11:23 +0900

    Fix compile error

--- src/main/java/org/nomadblacky/hamburger_shop/Main.java
+++ src/main/java/org/nomadblacky/hamburger_shop/Main.java
@@ -1,6 +1,5 @@
 package org.nomadblacky.hamburger_shop;
 
-import org.nomadblacky.hamburger_shop.hamburger.Hamburger;
 import org.nomadblacky.hamburger_shop.topping.NormalTopping;
 import org.nomadblacky.hamburger_shop.topping.Topping;
 
@@ -8,8 +7,6 @@ import org.nomadblacky.hamburger_shop.topping.Topping;
  * Created by blacky on 17/06/20.
  */
 public class Main {
-
-    private Hamburger regular = new
 
     private Topping tomato  = new NormalTopping("トマト",  30);
     private Topping lettuce = new NormalTopping("レタス",  75);
